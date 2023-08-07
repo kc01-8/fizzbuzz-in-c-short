12595eabbfa86266232bea998128045d2095bf25
kc01-8 <minecraftandcallofduty101@gmail.com>
2023-08-07 17:31:36 +0300

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int i;
-    for (i = 1; i < 101; i++){
+    for (int i = 1; i < 101; i++){
         if(i % 3 == 0 && i % 5 == 0){
             printf("FizzBuzz\n");
         } else if(i % 3 == 0){
